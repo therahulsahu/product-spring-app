fa4a5c1e124a0e65281faad4340fdd5cc286001d
Urvashi Prajapati <urvashi.prajapati@rakuten.com>
2022-07-15 20:59:50 +0530

    camunda changes

---
+++ product-service-camunda/start_process.sh
@@ -0,0 +1,9 @@
+echo "\nStart Creating Product Process"
+curl --location --silent --output --request POST 'http://localhost:8080/api/productlist/v1/createproduct' --header 'Content-Type: application/json' --data-raw '{
+     "variables": {
+         "order": {
+             "value": "Successfully Completed",
+             "type": "String"
+        }
+    }
+}'
