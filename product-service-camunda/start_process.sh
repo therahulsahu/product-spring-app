85a2e6c36f5abb44c407c7102bebb2716da89ed0
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2022-07-26 19:51:04 +0530

    Update start_process.sh

--- product-service-camunda/start_process.sh
+++ product-service-camunda/start_process.sh
@@ -2,7 +2,7 @@ echo "\nStart Creating Product Process"
 curl --location --silent --output --request POST 'http://localhost:8080/api/productlist/v1/createproduct' --header 'Content-Type: application/json' --data-raw '{
      "variables": {
          "order": {
-             "value": "Successfully Completed",
+             "value": "null",
              "type": "String"
         }
     }
